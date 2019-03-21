b2dce1a30874432f354352bd7c13eed300da5009
rabazan <rabazan@undec.edu.ar>
2019-03-21 17:04:45 -0300

    Seccion 4

--- clases/server.ts
+++ clases/server.ts
@@ -1,17 +1,46 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import sokectIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server{
 
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
+ 
+    public io: SocketIO.Server;
+    private httpServer: http.Server;
 
-    constructor(){
+    private constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = sokectIO(this.httpServer);
+        this.escucharSockets();
+    }
+
+    public static get instance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexion - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar(cliente);
+            
+        });
     }
 
     start(callback: Function){
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port);
     }
 }
---
+++ sockets/socket.ts
@@ -0,0 +1,19 @@
+import { Socket } from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+    cliente.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+}
+
+// Escuchar mensaje
+export const mensaje = (cliente: Socket, io: SocketIO.Server) => {
+ cliente.on('mensaje', (payload: {de: string, cuerpo: string} ) => {
+
+    console.log('Mensaje recibido', payload);
+    
+    io.emit('mensaje-nuevo', payload);
+
+ });
+}
